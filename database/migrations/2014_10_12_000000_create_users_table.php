091b3a52cac227171c90873b60e535f3e997d962
pocholo818 <89579920+pocholo818@users.noreply.github.com>
2023-04-17 16:29:43 +0800

    revert some stuff

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,9 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id('admin_id');
+            $table->id('id');
+            // $table->id('admin_id');
             $table->string('name', 128);
-            $table->string('username', 64);
+            // $table->string('username', 64);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
