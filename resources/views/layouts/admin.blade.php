5e88221f63af0e87cd4d36d463a909f20637fa36
Edrique0311 <55437786+reyesjustinedrique@users.noreply.github.com>
2023-04-20 15:31:24 +0800

    fixed admin user dropdown

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -94,10 +94,6 @@
                         </button>
 
 
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }}
-                        </a>
-
 
 
                         <div class="dropdown-menu dropdown-menu-end">
